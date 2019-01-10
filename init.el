1deaf657d4aeef173e5c5061317a60713cf098ff
wave-jumper <46484917+wave-jumper@users.noreply.github.com>
2019-01-10 15:38:08 +0000

    This is where the fun begins

---
+++ init.el
@@ -0,0 +1,15 @@
+(add-to-list 'auto-mode-alist '("\\.hts\\'" . html-mode))
+(load-theme 'tsdh-dark' t)
+(desktop-save-mode 1)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files (quote ("c:/Nodiadau/cwg.org"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
